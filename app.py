d5efa18433c0acbbc0ce7ace55b7671e505b39bc
Malik Saad Ahmed <161501277+Maliksaad231224@users.noreply.github.com>
2025-07-13 21:13:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,9 @@ def setup_driver():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-application-cache')
+    chrome_options.add_argument('--disable-extensions')
+    chrome_options.add_argument('--disable-default-apps')
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
